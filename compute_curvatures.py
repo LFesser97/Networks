ab6181fed42031801a3f8e26d12e09d1cbb02a48
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-03-28 08:30:42 -0400

    changed alpha in orc to 0

--- compute_curvatures.py
+++ compute_curvatures.py
@@ -228,7 +228,7 @@ def get_orc_edge_curvatures(G):
     -------
     None.
     """
-    orc = OllivierRicci(G, alpha=0.5, verbose="ERROR")
+    orc = OllivierRicci(G, alpha=0, verbose="ERROR") # might have to change this back to 0.5
     orc.compute_ricci_curvature()
 
     for (u, v) in list(orc.G.edges()):
